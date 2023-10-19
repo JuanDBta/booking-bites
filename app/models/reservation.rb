9bdcaff6c060a9103fe529c212e4e7d411f37300
beteland123 <beteland23@gmail.com>
2023-10-19 18:36:21 +0300

    Add relations and validation to the reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :section
   belongs_to :user
+  validates :city, presence: true
+  validates :date, presence: true
+  validates :number_of_person, numericality: { only_integer: true, greater_than: 1 }
 end
