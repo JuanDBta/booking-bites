965e35a70fe572238342ac3b2353f91489130e37
JuanDBta <simplebetsas@gmail.com>
2023-10-19 16:42:55 -0500

    Fixed offenses

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,34 +1,33 @@
 class ReservationsController < ApplicationController
-    def index
-      @reservations = Reservation.all
-    end
-  
-    def show
-      @reservation = Reservation.find(params[:id])
-    end
-  
-    def new
-      @reservation = Reservation.new
-    end
-  
-    def create
-      @reservation = Reservation.new(reservation_params)
-      if @reservation.save
-        redirect_to @reservation
-      else
-        render 'new'
-      end
-    end
-  
-    def api_index
-        @reservations = Reservation.all
-        render json: @reservations
-    end
+  def index
+    @reservations = Reservation.all
+  end
 
-    private
-  
-    def reservation_params
-      params.require(:reservation).permit(:city, :date, :number_of_person, :section_id, :user_id)
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to @reservation
+    else
+      render 'new'
     end
   end
-  
+
+  def api_index
+    @reservations = Reservation.all
+    render json: @reservations
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:city, :date, :number_of_person, :section_id, :user_id)
+  end
+end
