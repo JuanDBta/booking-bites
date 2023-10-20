773dad22064ce79b2382f47af783e53a52d43ef4
beteland123 <beteland23@gmail.com>
2023-10-20 15:25:51 +0300

    Ad index, show action for sections controller

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,14 @@
 class SectionsController < ApplicationController
+    def index
+        @sections = Section.all
+        render json: @sections, status: :ok
+    end
+    
+    def show
+        @section = Section.find(params[:id])
+        render json: @section, status: :ok
+    end
+
+   
 end
+    
