7e14338f0b350e8e41a951497763744b1e6e6419
beteland123 <beteland23@gmail.com>
2023-10-20 15:34:56 +0300

    Add action of new and create action of section

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,14 +1,36 @@
 class SectionsController < ApplicationController
     def index
         @sections = Section.all
-        render json: @sections, status: :ok
+        render json: @sections
     end
     
     def show
         @section = Section.find(params[:id])
-        render json: @section, status: :ok
+        render json: @section
     end
-
+    def new
+       
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        @section = Section.new(restaurant: @restaurant)
+    end
+    
+    def create
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        @section = Section.new(section_params)
+        @section.restaurant= @restaurant
+        if @section.save
+            render json: @section
+            redirect_to @section
+        else
+            render json: @section.errors
+        end
+      end
+    
+      private
+    
+      def section_params
+        params.require(:section).permit(:name, :image, :description, :capacity, :cover,:live_music, :restaurant_id, :user_id)
+      end
    
 end
     
