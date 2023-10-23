de53ec906d72ac7c59b810d7c2bd81e94cf37318
beteland123 <beteland23@gmail.com>
2023-10-24 00:08:36 +0300

    Use the useparams to get specfic section with its id

--- app/javascript/components/Detail.js
+++ app/javascript/components/Detail.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 function SectionDetail() {
-   
+    const sections = useSelector((state) => state.sections);
+    const { id } = useParams();
+    const detailsection = sections.find((section) => section.id === Number(id));
 }
