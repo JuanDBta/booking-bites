a3d8aa296313f2c727755e9bbb1a2319c7ae9211
beteland123 <beteland23@gmail.com>
2023-10-24 00:13:30 +0300

    List the deatils

--- app/javascript/components/Detail.js
+++ app/javascript/components/Detail.js
@@ -5,4 +5,26 @@ function SectionDetail() {
     const sections = useSelector((state) => state.sections);
     const { id } = useParams();
     const detailsection = sections.find((section) => section.id === Number(id));
-}
+    if (!detailsection) {
+        return <div>Loading...</div>; 
+      }
+      return (
+        <div className='detail_container flex'>
+          <img src={detailsection.image} alt="Section Image" />
+          
+          
+              <h2 className='discover flex'>{detailsection.name}</h2>
+              
+              <p>{detailsection.description}</p>
+            
+              <p> {detailsection.capacity}</p>
+            
+              <p>{detailsection.cover ? 'Yes' : 'No'}</p>
+            
+              <p> {detailsection.live_music ? 'Yes' : 'No'}</p>
+            
+        </div>
+      );
+    }
+    
+    export default SectionDetail;
