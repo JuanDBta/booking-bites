9d58b32b571566e06890f282a3f11eb854566408
JuanDBta <simplebetsas@gmail.com>
2023-10-24 18:17:39 -0500

    Added hover to active link

--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import '../../assets/stylesheets/navbar.css';
 
 function NavBar() {
+  const location = useLocation();
   return (
     <nav className="navbar">
-      <li className="nav-bar-link"><NavLink to="/">SECTIONS</NavLink></li>
-      <li className="nav-bar-link"><NavLink to="/sections/new">ADD SECTION</NavLink></li>
-      <li className="nav-bar-link"><NavLink to="/reservations/new">ADD RESERVATION</NavLink></li>
-      <li className="nav-bar-link"><NavLink to="/reservations">MY RESERVATIONS</NavLink></li>
+      <li className={`nav-bar-link ${location.pathname === '/' ? 'active' : ''}`}>
+        <NavLink to="/">SECTIONS</NavLink>
+      </li>
+      <li className={`nav-bar-link ${location.pathname === '/sections/new' ? 'active' : ''}`}>
+        <NavLink to="/sections/new">ADD SECTION</NavLink>
+      </li>
+      <li className={`nav-bar-link ${location.pathname === '/reservations/new' ? 'active' : ''}`}>
+        <NavLink to="/reservations/new">ADD RESERVATION</NavLink>
+      </li>
+      <li className={`nav-bar-link ${location.pathname === '/reservations' ? 'active' : ''}`}>
+        <NavLink to="/reservations">MY RESERVATIONS</NavLink>
+      </li>
     </nav>
   );
 }
