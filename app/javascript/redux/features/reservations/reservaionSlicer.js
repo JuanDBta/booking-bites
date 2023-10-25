43ba219b95694997ae260a553299bb7a88fbd475
beteland123 <beteland23@gmail.com>
2023-10-25 18:56:38 +0300

    Add the APi endpont for post and etxrareducer

--- app/javascript/redux/features/reservations/reservaionSlicer.js
+++ app/javascript/redux/features/reservations/reservaionSlicer.js
@@ -2,12 +2,22 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const url = 'http://localhost:3000/api/sections';
+const res= 'reservations'
 const initialState = {
   reservations: [],
   status: false,
   error: '',
 };
 
+
+export const addReservation = createAsyncThunk('reservations/addReservation', async (newtable) => {
+  try {
+    await axios.post(`${url}/${newtable.id}/${res}`,newtable);
+    return newtable;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 
 
@@ -17,8 +27,13 @@ const reservationSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
    
+    builder.addCase(addReservation.fulfilled, (state, action) => {
+      state.reservations.push(action.payload);
+    });
   },
 });
-
+export const getReservations = (state) => state.reservations.reservations;
+export const getStatus = (state) => state.reservations.status;
+export const getError = (state) => state.reservations.error;
 
 export default reservationSlice.reducer;
