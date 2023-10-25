59847c080a673dae5d541862086358a34a563928
beteland123 <beteland23@gmail.com>
2023-10-25 19:20:04 +0300

    Add reservation reducer to the store

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sectionsReducer from './features/sections/sectionsSlice';
-
+import reservationSlice from'./features/reservations/reservaionSlicer';
 const store = configureStore({
   reducer: {
     sections: sectionsReducer,
+    reservations: reservationSlice
   },
 });
 
