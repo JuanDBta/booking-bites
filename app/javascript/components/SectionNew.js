ff34237db8096fa3dec55d6cf7862fc572a47a27
beteland123 <beteland23@gmail.com>
2023-10-26 21:11:00 +0300

    Add condition for selecting restaurnt or if ther is no restuarnat to creat one

--- app/javascript/components/SectionNew.js
+++ app/javascript/components/SectionNew.js
@@ -1,42 +1,44 @@
-import React from 'react';
-import '../../assets/stylesheets/sectionnew.css';
-import { useEffect,useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { FaBars, FaSearch } from 'react-icons/fa';
-import { addSection} from '../redux/features/sections/sectionsSlice'
-import { fetchrestaurants } from '../redux/features/restaurants/restaurantSlicer';
+import { addSection } from '../redux/features/sections/sectionsSlice';
+import { fetchrestaurants} from '../redux/features/restaurants/restaurantSlicer';
+import '../../assets/stylesheets/sectionnew.css';
+import { Link } from 'react-router-dom';
+
 function SectionNew() {
   const dispatch = useDispatch();
   const restaurants = useSelector((state) => state.restaurants);
   const [sectionData, setSectionData] = useState({
     name: '',
-   image: '',
-   description: '',
+    image: '',
+    description: '',
     capacity: '',
     cover: '',
-    live_music:'',
+    live_music: '',
     restaurant_id: '',
-  })
+  });
+
   const handleChange = (e) => {
     setSectionData({
       ...sectionData,
       [e.target.name]: e.target.value,
     });
   };
+
   useEffect(() => {
     dispatch(fetchrestaurants());
   }, [dispatch]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newSection= {
+    const newSection = {
       name: sectionData.name,
       image: sectionData.image,
       description: sectionData.description,
       capacity: sectionData.capacity,
-      cover:sectionData.cover,
-      live_music:sectionData.live_music,
-      restaurant_id:sectionData.restaurant_id
+      cover: sectionData.cover,
+      live_music: sectionData.live_music,
+      restaurant_id: sectionData.restaurant_id,
     };
     dispatch(addSection(newSection)).unwrap();
     setSectionData({
@@ -45,72 +47,91 @@ function SectionNew() {
       description: '',
       capacity: '',
       cover: '',
-      live_music:'',
+      live_music: '',
       restaurant_id: '',
     });
   };
+
   return (
-  <form onSubmit={handleSubmit} className='flex'>
-    <div class="form_row">
-    <input
-      type="text"
-      name="name"
-      value={sectionData.name}
-      onChange={handleChange}
-      placeholder="Name"
-      className='input'
-    />
-    <input
-      type="textarea"
-      name="description"
-      value={sectionData.description}
-      onChange={handleChange}
-      placeholder="Description"
-      className='input'
-    />
-     <input
-      type="boolean"
-      name="cover"
-      value={sectionData.cover}
-      onChange={handleChange}
-      placeholder="Cover"
-      className='input'
-    />
-     <input
-      type="boolean"
-      name="live_music"
-      value={sectionData.cover}
-      onChange={handleChange}
-      placeholder="Live music"
-      className='input'
-    />
-    <input
-      type="number"
-      name="capacity"
-      value={sectionData.capacity}
-      onChange={handleChange}
-      min={1}
-      placeholder="Capacity"
-      className='input'
-    />
-    <select
-    name="restaurant_id"
-    value={sectionData.restaurant_id}
-     onChange={handleChange}
-     className='input'
-     id="select"
-     
-     >
-     <option value="">Select a restaurant</option>
-          {restaurants.map((restaurant) => (
-          <option key={restaurant.id} value={restaurant.id}>
-            {restaurant.name}
-          </option>
-          ))}
-    </select>
-     <button type="submit" className='flex'>Create</button>
-     </div>
-  </form>
+    <form onSubmit={handleSubmit} className="flex" id="selection_form">
+      {restaurants.length > 0 ? (
+        <>
+          <input
+            type="text"
+            name="name"
+            value={sectionData.name}
+            onChange={handleChange}
+            placeholder="Name"
+            className="input"
+          />
+          <input
+            type="textarea"
+            name="description"
+            value={sectionData.description}
+            onChange={handleChange}
+            placeholder="Description"
+            className="input"
+          />
+          <input
+            type="checkbox"
+            name="cover"
+            checked={sectionData.cover}
+            onChange={handleChange}
+            className="input"
+          />
+          <label htmlFor="cover">Cover</label>
+          <input
+            type="checkbox"
+            name="live_music"
+            checked={sectionData.live_music}
+            onChange={handleChange}
+            className="input"
+          />
+          <label htmlFor="live_music">Live music</label>
+          <input
+            type="number"
+            name="capacity"
+            value={sectionData.capacity}
+            onChange={handleChange}
+            min={1}
+            placeholder="Capacity"
+            className="input"
+          />
+
+          <select
+            name="restaurant_id"
+            value={sectionData.restaurant_id}
+            onChange={handleChange}
+            className="input"
+            id="select"
+          >
+            <option value="">Select a restaurant</option>
+            {restaurants.map((restaurant) => (
+              <option key={restaurant.id} value={restaurant.id}>
+                {restaurant.name}
+              </option>
+            ))}
+          </select>
+      <button type="submit" className="flex">
+        Create
+      </button>
+        </>
+      ) : (
+        <>
+          <p className="notice">Please create a restaurant first!</p>
+          <Link to="/restaurant/new">
+            <p className="flex discover">
+              <button className="detail_reserve flex">
+                Create restaurant
+                <div className="circle-right">
+                  <p>&gt;</p>
+                </div>
+              </button>
+            </p>
+          </Link>
+        </>
+      )}
+    </form>
   );
 }
 
