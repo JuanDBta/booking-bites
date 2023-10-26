05a01915a5b0d779b9b9981ff6357b43c8b486ed
JuanDBta <simplebetsas@gmail.com>
2023-10-26 14:36:50 -0500

    Added section to reservations info

--- app/javascript/components/Reservations.js
+++ app/javascript/components/Reservations.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchReservationsApi } from '../redux/features/reservations/reservationsApiSlice';
+import { fetchSections } from '../redux/features/sections/sectionsSlice'; // Asegúrate de importar la acción para obtener las secciones
 import '../../assets/stylesheets/reservations.css';
 
 function Reservations() {
   const dispatch = useDispatch();
   const reservationsApi = useSelector((state) => state.reservationsApi);
+  const sections = useSelector(state => state.sections);
 
   useEffect(() => {
     dispatch(fetchReservationsApi());
+    dispatch(fetchSections()); // Obtén las secciones
   }, [dispatch]);
 
+  const getSectionName = (sectionId) => {
+    const section = sections.find(section => section.id === sectionId);
+    return section ? section.name : 'Unknown Section'; // Maneja el caso en el que la sección no se encuentra
+  };
 
   return (
     <div className="reservations-container">
@@ -18,12 +25,14 @@ function Reservations() {
       <h3 className="title-description">List of your reservations</h3>
       <div className="dotted-line"></div>
       <ul className="reservations-list">
-        {reservationsApi.map((reservation) => (
-        <li key={reservation.id} className="info">
-            <p>Reservation #{reservation.id}</p>
-            <p>City: {reservation.city}</p>
-            <p>Date: {reservation.date}</p>
-            <p>Number of Persons: {reservation.number_of_person}</p>
+        {reservationsApi.map((reservation, index) => (
+        <li key={index} className="info">
+            <p className="text">Reservation #{index + 1}:</p>
+            <p className="text desc">ID {reservation.id}</p>
+            <p className="text desc">{reservation.date}</p>
+            <p className="text desc">{reservation.city}</p>
+            <p className="text desc">{getSectionName(reservation.section_id)}</p>            
+            <p className="text desc"># People: {reservation.number_of_person}</p>
         </li>
         ))}
       </ul>
